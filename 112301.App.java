ed4f7c1f34bbb540a7cf442339bc5612f4049c71
QzhuB <57722671+QzhuHB@users.noreply.github.com>
2020-11-23 11:45:50 +0800

    Update 112301.App.java
    随机坐标

--- 112301.App.java
+++ 112301.App.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 
 public class MathTest {
     public static void main(String[] args) {
-        int x, y;
+        int x,y,z;
         while (true) {
             Scanner scanner = new Scanner(System.in);
             x = scanner.nextInt();
+            if (x==1){
+            z = (int) (Math.random() * 100);
             y = (int) (Math.random() * 100);
-            System.out.println(x + "," + y);
-        }
+            System.out.println(z + "," + y);
+        }else {
+                System.out.println("输入的不是1");
+            }
+    }
     }
 }
