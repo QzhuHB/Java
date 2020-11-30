0e4968611c671a3aabe599b4dc9fa88bbeca5e4d
QzhuB <57722671+QzhuHB@users.noreply.github.com>
2020-11-30 11:29:35 +0800

    Create 113002.接口.R.java

---
+++ 113002.接口.R.java
@@ -0,0 +1,12 @@
+package java11028;
+
+public interface R {
+	static String boy_filePath="img/boy.gif";
+	static String girl_filePath="img/girl.gif";
+	static int [][]arr={{1,2,3},{4,5,6}};
+	static void sout() {
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		System.out.println(arr);
+	}
+}
