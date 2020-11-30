98ea3930c21fd30328b2bbefdf3ab4990c0021a4
QzhuB <57722671+QzhuHB@users.noreply.github.com>
2020-11-30 11:25:51 +0800

    Create 113001.继承子类.App.java

---
+++ 113001.继承子类.App.java
@@ -0,0 +1,10 @@
+package java11028;
+
+public class R {
+	String boy_filePath="img/boy.gif";
+	String girl_filePath="img/girl.gif";
+	int [][]arr={{1,2,3},{4,5,6}};
+	static void sout() {
+		
+	}
+}
