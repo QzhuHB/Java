4cdd625aa06ec47555b3c816d217b59428b2d995
QzhuB <57722671+QzhuHB@users.noreply.github.com>
2020-11-30 11:27:24 +0800

    Update 113001.继承子类.App.java

--- 113001.继承子类.App.java
+++ 113001.继承子类.App.java
@@ -1,10 +1,12 @@
 package java11028;
 
 public class R {
-	String boy_filePath="img/boy.gif";
-	String girl_filePath="img/girl.gif";
-	int [][]arr={{1,2,3},{4,5,6}};
+	static String boy_filePath="img/boy.gif";
+	static String girl_filePath="img/girl.gif";
+	static int [][]arr={{1,2,3},{4,5,6}};
 	static void sout() {
-		
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		System.out.println(arr);
 	}
 }
